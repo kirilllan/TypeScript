7814fcf89ca15dfa49a5aa1db00a8295f26da3ca
kirilllan <s2100138@edu.bc.fi>
2021-11-13 18:17:17 +0200

    tuple example

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log('your code goes here')
+//tuple, when you need exactly x amount of z,x,y types
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: 'somename',
+  age: 1,
+  hobbies: ['eat', 'walk'],
+  role: [2, 'author']
+}
