983256495564913616bd417f89f233fd3779b767
kirilllan <s2100138@edu.bc.fi>
2021-11-13 18:27:57 +0200

    Enum explanation

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-//tuple, when you need exactly x amount of z,x,y types
+//Tuple, when you need exactly x amount of z,x,y types
 const person: {
   name: string;
   age: number;
@@ -9,4 +9,13 @@ const person: {
   age: 1,
   hobbies: ['eat', 'walk'],
   role: [2, 'author']
+}
+
+//Enum, human-readable identifiers that have mapped values behind the scenes. Constants you can easily relate to.
+enum Role { ADMIN = 10, READ_ONLY, AUTHOR };
+const person2 = {
+  name: 'somename',
+  age: 1,
+  hobbies: ['eat', 'walk'],
+  role: Role.ADMIN
 }
