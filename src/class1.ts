63c0b25270edb9c5dfc0a81e114c09e8f9586a40
kirilllan <s2100138@edu.bc.fi>
2021-11-17 13:30:00 +0200

    tinkering with this kw in class1

--- src/class1.ts
+++ src/class1.ts
@@ -3,7 +3,18 @@ class Department {
   constructor(n: string) {
     this.name = n
   }
+  describe(this: Department) {
+    console.log('Department: ' + this.name)
+  }
 }
 
 const accounting = new Department('Accounting')
-console.log(accounting)
+//console.log(accounting)
+
+//const accountingCopy = {describe: accounting.describe}
+// accountingCopy.describe()//->undefined. here 'this' refers to a thing calling the method
+//->after making TS change, you will get an error.
+//but code below is ok
+const accountingCopy2 = { name: 'this is ok now', describe: accounting.describe }
+accountingCopy2.describe()
+//
