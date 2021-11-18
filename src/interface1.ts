3ad8ae9d5a3ed17bc3e72fe158dd15d862bcd05f
kirilllan <s2100138@edu.bc.fi>
2021-11-18 13:20:17 +0200

    implementing a class from an interface

--- src/interface1.ts
+++ src/interface1.ts
@@ -4,5 +4,19 @@ interface Person {
   greet(phrase: string): void
 }
 
+class Person1 implements Person {
+  name: string
+  age = 30
+  maxAge = 3000
+  constructor(n: string) {
+    this.name = n
+  }
+  greet() {
+    return
+  }
+}
+
 let user1: Person
 user1 = { name: 'somename', age: 0, greet() { } }
+
+let user2 = new Person1('mm')
