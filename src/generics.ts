c2d0a4fcd817490d99847f78f8ef1e4f3d3a1618
kirilllan <s2100138@edu.bc.fi>
2021-11-20 16:38:44 +0200

    second generic type

--- src/generics.ts
+++ src/generics.ts
@@ -1,1 +1,10 @@
+// better type safety. additional type info.
 const names: Array<string | number> = []
+
+const promise: Promise<string> = new Promise((res, rej) => {
+  setTimeout(() => {
+    res('This is done ')
+  }, 2000)
+})
+
+promise.then(data => data.split(''))
