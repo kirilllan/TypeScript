0a5dd174615788b8aa055254ae4cd22ddf26421c
kirilllan <s2100138@edu.bc.fi>
2021-11-21 12:29:10 +0200

    wrote a generic function

--- src/generics.ts
+++ src/generics.ts
@@ -12,6 +12,7 @@ promise.then(data => data.split(''))
 
 //
 //const mergedObj = merge({name: 'na1'}, {age: 0}) as {name:string, age: number}
-function merge<T, U>(objA: T, objB: U) {
+function merge<T, U>(objA: T, objB: U) { // TS knows return is of type T & U
   return Object.assign(objA, objB)
 }
+const mergedObj = merge({ name: 'na1' }, { age: 0 })
