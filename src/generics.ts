b96db8d53fc6b87e30ebdbad81042b35e0fb33df
kirilllan <s2100138@edu.bc.fi>
2021-11-21 12:19:37 +0200

    tryna build generic fn

--- src/generics.ts
+++ src/generics.ts
@@ -3,8 +3,15 @@ const names: Array<string | number> = []
 
 const promise: Promise<string> = new Promise((res, rej) => {
   setTimeout(() => {
-    res('This is done ')
+    res('')
   }, 2000)
 })
 
 promise.then(data => data.split(''))
+
+
+//
+//const mergedObj = merge({name: 'na1'}, {age: 0}) as {name:string, age: number}
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB)
+}
