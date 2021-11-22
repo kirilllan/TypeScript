c48f0991d727c38b45c215a890c0bbd3b88bb7dc
kirilllan <s2100138@edu.bc.fi>
2021-11-22 13:34:30 +0200

    decorator rendering txt

--- src/decorators.ts
+++ src/decorators.ts
@@ -5,7 +5,17 @@ function Logger(logString: string) {
   }
 }
 
-@Logger('from decorator ')
+function WithTemplate(template: string, hookId: string) {
+  return function (_: Function) {
+    const hookElement = document.getElementById(hookId)
+    if (hookElement) {
+      hookElement.innerHTML = template
+    }
+  }
+}
+
+//@Logger('from decorator ') //logString
+@WithTemplate("<h1>bub</h1>", "app")
 class Person {
   name = " "
   constructor() { }
