12a4cea23cabc72161bb56569afc8a86be5f93b5
kirilllan <s2100138@edu.bc.fi>
2021-11-23 10:48:48 +0200

    created base Product class

--- src/decorators.ts
+++ src/decorators.ts
@@ -24,3 +24,20 @@ class Person {
 }
 
 const person1 = new Person()
+
+
+// property decorators
+class Product {
+  title: string
+  private _price: number
+  set price(val: number) {
+    if (val > 0) this._price = val
+  }
+  constructor(t: string, p: number) {
+    this.title = t
+    this._price = p
+  }
+  getPriceWithTax(tax: number): number {
+    return this._price * (1 + tax)
+  }
+}
