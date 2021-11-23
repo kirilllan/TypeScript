a1655e324ec04bf299fbd84ce8e333d59fdd7803
kirilllan <s2100138@edu.bc.fi>
2021-11-23 13:32:53 +0200

    stargint input validation with decorators

--- src/decorators.ts
+++ src/decorators.ts
@@ -111,3 +111,25 @@ class Printer {
 }
 const p = new Printer()
 document.querySelector('button')!.addEventListener('click', p.showMessage)
+
+
+// validation with decorators
+class Course {
+  title: string
+  price: number
+  constructor(t: string, p: number) {
+    this.title = t
+    this.price = p
+  }
+}
+
+const form = document.querySelector('form')!
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const titleEl = document.getElementById('title') as HTMLInputElement
+  const priceEl = document.getElementById('price') as HTMLInputElement
+  const title = titleEl.value
+  const price = +priceEl.value
+
+  const created = new Course(title, price)
+})
