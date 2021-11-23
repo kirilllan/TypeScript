685d6f0aa59ff217567fd825855bffa8f2ec1ac5
kirilllan <s2100138@edu.bc.fi>
2021-11-23 11:15:45 +0200

    added accessor and parameter decorators

--- src/decorators.ts
+++ src/decorators.ts
@@ -26,15 +26,30 @@ class Person {
 const person1 = new Person()
 
 
-// property decorators
+// more different decorators
 function Log(target: any, propertyName: string | Symbol) {
   console.log(target, propertyName)//target is Class, propertyName is 'title' in this case
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log(target, name, descriptor)
+  // target is prototype with constructor, setter and method, name is 'price' in this case, descriptor is PropertyDescriptor where get is undefined, set is a function and configurable === true and enumerable === false
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log(target, name, descriptor)
+  // target is prototype, name is getPriceWithTax, descriptor is PropertyDescriptor with value prop being the function, configurable === true, enumerable === false, writable === true
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  //name is getPriceWithTax position is arguments position, 0
 }
 
 class Product {
   @Log
   title: string
   private _price: number
+  @Log2
   set price(val: number) {
     if (val > 0) this._price = val
   }
@@ -42,7 +57,8 @@ class Product {
     this.title = t
     this._price = p
   }
-  getPriceWithTax(tax: number): number {
+  @Log3
+  getPriceWithTax(@Log4 tax: number): number {
     return this._price * (1 + tax)
   }
 }
