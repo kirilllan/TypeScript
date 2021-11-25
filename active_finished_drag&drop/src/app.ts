65f2e20c4f65291b0dcca7b6b6be0c8f0a1f4bae
kirilllan <s2100138@edu.bc.fi>
2021-11-25 12:41:59 +0200

    importing a node

--- active_finished_drag&drop/src/app.ts
+++ active_finished_drag&drop/src/app.ts
@@ -4,6 +4,10 @@ class ProjectInput {
   constructor() {
     this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!
     this.hostElement = document.getElementById('app')! as HTMLDivElement
+
+    const importedNode = document.importNode(this.templateElement.content, true)
+
+
   }
 }
 // getting access to template
