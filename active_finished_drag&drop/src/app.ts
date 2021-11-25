b999f4a01a970666e360d4a7f1199b8b0b0ea433
kirilllan <s2100138@edu.bc.fi>
2021-11-25 12:39:47 +0200

    got access to two html els

--- active_finished_drag&drop/src/app.ts
+++ active_finished_drag&drop/src/app.ts
@@ -1,8 +1,9 @@
 class ProjectInput {
   templateElement: HTMLTemplateElement
+  hostElement: HTMLDivElement
   constructor() {
     this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!
-    this.hostElement = 
+    this.hostElement = document.getElementById('app')! as HTMLDivElement
   }
 }
 // getting access to template
