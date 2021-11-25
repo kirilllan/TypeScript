99b7a9e88e1e6afd408b38ef80a3254e29926f07
kirilllan <s2100138@edu.bc.fi>
2021-11-25 12:36:44 +0200

    getting access to template

--- active_finished_drag&drop/src/app.ts
+++ active_finished_drag&drop/src/app.ts
@@ -1,1 +1,8 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement
+  constructor() {
+    this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!
+    this.hostElement = 
+  }
+}
+// getting access to template
