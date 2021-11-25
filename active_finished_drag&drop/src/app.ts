171838092e8f2641d288261cada52796586da1ef
kirilllan <s2100138@edu.bc.fi>
2021-11-25 12:49:28 +0200

    finished class ProjectInput

--- active_finished_drag&drop/src/app.ts
+++ active_finished_drag&drop/src/app.ts
@@ -1,13 +1,16 @@
 class ProjectInput {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
+  element: HTMLFormElement
   constructor() {
     this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!
     this.hostElement = document.getElementById('app')! as HTMLDivElement
 
     const importedNode = document.importNode(this.templateElement.content, true)
-
-
+    this.element = importedNode.firstElementChild as HTMLFormElement
+    this.attach()
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element)
   }
 }
-// getting access to template
