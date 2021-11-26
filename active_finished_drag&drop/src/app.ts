e88d552bb6b620a4d5536239723fa973327d9a09
kirilllan <s2100138@edu.bc.fi>
2021-11-26 12:38:56 +0200

    renderContent and attach methods

--- active_finished_drag&drop/src/app.ts
+++ active_finished_drag&drop/src/app.ts
@@ -114,6 +114,15 @@ class ProjectList {
 
     const importedNode = document.importNode(this.templateElement.content, true)
     this.element = importedNode.firstElementChild as HTMLElement
-    this.element.id = ''
+    this.element.id = `${this.type}-projects`
+    this.attach()
+  }
+  private renderContent() {
+    const listId = `${this.type}-projects-list`
+    this.element.querySelector('ul')!.id = listId
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' projects'
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element)
   }
 }
