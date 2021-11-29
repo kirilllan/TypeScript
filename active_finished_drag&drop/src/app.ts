40fc9170273f08f92f9c259d3f87e4213bd03831
kirilllan <s2100138@edu.bc.fi>
2021-11-29 13:28:35 +0200

    class Project

--- active_finished_drag&drop/src/app.ts
+++ active_finished_drag&drop/src/app.ts
@@ -1,3 +1,9 @@
+enum ProjectStatus { Active, Finished }
+
+class Project {
+  constructor(public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) { }
+}
+
 class ProjectState {
   private listeners: any[] = []
   private projects: any[] = []
