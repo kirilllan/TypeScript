97e163c4ac8d3aee139fbe051bf489f3dc691cf5
kirilllan <s2100138@edu.bc.fi>
2021-11-30 11:25:18 +0200

    constructor for Component

--- active_finished_drag&drop/src/app.ts
+++ active_finished_drag&drop/src/app.ts
@@ -136,6 +136,18 @@ class ProjectInput {
   }
 }
 
+class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement
+  hostElement: T
+  element: U
+  constructor(templateId: string, hostElementId: string, newElementId?: string) {
+    this.templateElement = <HTMLTemplateElement>document.getElementById(templateId)! as HTMLTemplateElement
+    this.hostElement = document.getElementById(hostElementId)! as T
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as U
+    if (newElementId) this.element.id = newElementId
+  }
+}
 
 class ProjectList {
   templateElement: HTMLTemplateElement
