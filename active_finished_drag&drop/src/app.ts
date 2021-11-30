b6c26d830b7b5641dc5993ac87ea5c16e6845261
kirilllan <s2100138@edu.bc.fi>
2021-11-30 13:09:05 +0200

    refactor ProjectState with State

--- active_finished_drag&drop/src/app.ts
+++ active_finished_drag&drop/src/app.ts
@@ -4,20 +4,23 @@ class Project {
   constructor(public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) { }
 }
 
-type Listener = (items: Project[]) => void
+type Listener<T> = (items: T[]) => void
 
-class ProjectState {
-  private listeners: Listener[] = []
+class State<T> {
+  protected listeners: Listener<T>[] = []
+  addListener(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn)
+  }
+}
+
+class ProjectState extends State<Project> {
   private projects: Project[] = []
   private static instance: ProjectState
-  private constructor() { }
+  private constructor() { super() }
   static getInstance() {
     if (this.instance) return this.instance
     this.instance = new ProjectState
     return this.instance
-  }
-  addListener(listenerFn: Listener) {
-    this.listeners.push(listenerFn)
   }
   addProject(title: string, description: string, numOfPeople: number) {
     const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active)
