66f05171fbc535aeffe4bc1a0e09aedfa63f0700
kirilllan <s2100138@edu.bc.fi>
2021-12-01 12:19:09 +0200

    interfaces Draggable and DragTarget

--- active_finished_drag&drop/src/app.ts
+++ active_finished_drag&drop/src/app.ts
@@ -1,3 +1,14 @@
+interface Draggable {
+  dragStartHandler(event: DragEvent): void
+  dragEndHandler(event: DragEvent): void
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void
+  dropHandler(event: DragEvent): void
+  dragLeaveHandler(event: DragEvent): void
+}
+
 enum ProjectStatus { Active, Finished }
 
 class Project {
